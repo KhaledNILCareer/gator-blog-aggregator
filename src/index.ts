ba02b6d2d0783322a3f0ab16919cac443cad1c61
KhaledNILCareer <KhaledNILCareer@gmail.com>
2026-09-21 13:18:24 +0300

    feat: add CLI command handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import { readConfig, setUser } from "./config.js";
+import { CommandsRegistry, handlerLogin, registerCommand, runCommand } from "./commands.js";
 
 function main() {
-  setUser("Khaled");
-  console.log(readConfig())
+  const processArgs = process.argv.slice(2);
+  if (processArgs.length === 0) {
+    console.error("Not enough arguments");
+    process.exit(1);
+  }
+  const cmdName = processArgs[0];
+  const args = processArgs.slice(1);
+  const registry :CommandsRegistry = {};
+  registerCommand(registry, "login", handlerLogin)
+
+  try {
+    runCommand(registry, cmdName, ...args)
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("An unknown error occurred");
+    }
+    
+    process.exit(1);
+  }
+
+  
 }
 
 main();
